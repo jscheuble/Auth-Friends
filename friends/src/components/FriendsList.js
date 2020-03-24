445c663c3b6d467ca3ab241e9123cb81a3c624ba
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-03-24 15:42:08 -0700

    friends list component can view, add and delete friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
+import Nav from "./Nav";
+
 class FriendsList extends React.Component {
   state = {
-    friends: []
-    // addFriend: {
-    //     name: '',
-    //     age: '',
-    //     email: '',
-    //     id: Date.now()
-    // }
+    friends: [],
+    addFriend: {
+      name: "",
+      age: "",
+      email: "",
+      id: Date.now()
+    }
   };
 
   fetchFriends = () => {
@@ -24,12 +26,86 @@ class FriendsList extends React.Component {
       .catch(err => console.log(err));
   };
 
+  handleChange = e => {
+    this.setState({
+      addFriend: {
+        ...this.state.addFriend,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  addNewFriend = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/friends", this.state.addFriend)
+      .then(res => {
+        this.setState({ friends: res.data });
+        console.log(this.state.friends);
+      })
+      .catch(err => console.log(err));
+  };
+
+  deleteFriend = (e, id) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .delete(`/api/friends/${id}`)
+      .then(res => {
+        this.setState({
+          friends: res.data.filter(friend => friend.id !== id)
+        });
+        console.log(this.state.friends);
+      });
+  };
+
   componentDidMount() {
     this.fetchFriends();
   }
 
   render() {
-    return <div></div>;
+    return (
+      <>
+        <Nav />
+        <form onSubmit={this.addNewFriend}>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="age"
+            placeholder="age"
+            onChange={this.handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="e-mail"
+            onChange={this.handleChange}
+          />
+          <button type="submit">add new friend</button>
+        </form>
+        <div>
+          {this.state.friends.map(friend => {
+            return (
+              <div className="friend" key={friend.id}>
+                <p>Name: {friend.name}</p>
+                <p>Age: {friend.age}</p>
+                <p>Email: {friend.email}</p>
+                <button
+                  className="x"
+                  onClick={e => this.deleteFriend(e, friend.id)}
+                >
+                  x
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
   }
 }
 
